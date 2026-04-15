9b4f146f0ee055b05ea283a2fe158b2397775476
Ilmoretto <morete.alencar@gmail.com>
2026-04-15 15:05:52 -0400

    fix: configuração banco de dados

---
+++ DataContexts/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace AppEscolaDeMusica.DataContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using AppEscolaDeMusica.DataContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("mysql");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32)))
+    );
+
+builder.Services.AddControllers().AddJsonOptions(
+    options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    }); // Evita o erro de loop infinito na serialização JSON, e formata o JSON de forma legível
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,3 +39,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
